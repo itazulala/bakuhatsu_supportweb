3ab00b51f0610ac2b5032a665e2ebea99118cefa
itazulala <minaton.tutan@gmail.com>
2022-10-07 22:50:54 +0900

    javascript処理の共通化

--- static/assets/js/main.js
+++ static/assets/js/main.js
@@ -34,12 +34,73 @@ document.addEventListener('DOMContentLoaded', () => {
 
 document.getElementById('js-elem'); // トリガーになる要素を取得
 
+// フォームの値を取得する
+function getFormValue (elements) {
+  let parameters = []
+  for (let i = 0; i < elements.length; i++) {
+    if (!elements[i].id) continue
+    parameters.push({param_key: elements[i].id, param_value:elements[i].value})
+  }
+  return parameters
+}
+
+// リクエストクエリパラメータを作成する
+function createQueryParameter (parameters) {
+  let queryParameter = ''
+  parameters.forEach( function(value, index) {
+    queryParameter += value.param_key
+    queryParameter += '='
+    queryParameter += value.param_value
+    if (index < parameters.length - 1) queryParameter += '&'
+  })
+  return queryParameter
+}
+
+function sendRequest (parameter) {
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', 'http://127.0.0.1:8000/api/explosion/blast_calc/?' + parameter, false);
+  xhr.setRequestHeader('content-type', 'application/x-www-form-urlencoded;charset=UTF-8');
+  xhr.send()
+  return xhr.responseText
+}
+
+function addTableRowValue (res) {
+  const body_row = document.getElementById("body_row")
+  const table_body = document.getElementById("table_body")
+  res.results.forEach(function(array,index)  {
+    let element
+    if(index === 0) {
+      element = body_row
+    } else {
+      element = body_row.cloneNode(true)
+    }
+    array.forEach(function(value,index) {
+      if(index === 0) {
+        element.childNodes[index].innerText = value.toFixed(2)
+      } else {
+        element.childNodes[index].innerText = value.toExponential(2)
+      }
+
+    })
+    table_body.lastChild.after(element)
+  })
+}
+
+// function calculationExec () {
+//   const parameter = createQueryParameter(getFormValue(document.forms[0].elements))
+//   const res = JSON.parse(sendRequest(parameter))
+//   console.log(res)
+//   result_img.src = 'data:image/png;base64,' + res.result_img
+//   addTableRowValue(res)
+// }
+
 function calculationExec () {
   const exp_weight = document.getElementById('exp_weight')
   const exp_er = document.getElementById('exp_er')
   const add_tnt = document.getElementById('add_tnt')
   const meas_points = document.getElementById('meas_points')
   const parameter = 'exp_weight=' + exp_weight.value + '&exp_er=' + exp_er.value + '&add_tnt=' + add_tnt.value + '&meas_points=' + meas_points.value
+
   const xhr = new XMLHttpRequest();
   xhr.open('GET', 'http://127.0.0.1:8000/api/explosion/blast_calc/?' + parameter, true);
   xhr.setRequestHeader('content-type', 'application/x-www-form-urlencoded;charset=UTF-8');
@@ -51,6 +112,7 @@ function calculationExec () {
       result_img.src = 'data:image/png;base64,' + res.result_img
 
       const body_row = document.getElementById("body_row")
+      const table_body = document.getElementById("table_body")
       res.results.forEach(function(array,index)  {
         let element
         if(index === 0) {
@@ -66,8 +128,9 @@ function calculationExec () {
           }
 
         })
-        body_row.after(element);
+        table_body.lastChild.after(element)
       })
     }
   }
 }
+
